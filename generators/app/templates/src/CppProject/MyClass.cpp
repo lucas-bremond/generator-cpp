3b7b0534a5349da0f0043c3e43df593456e4d178
Lucas <lucas@axelspace.com>
2017-12-01 11:25:02 +0900

    misc: improve code coverage script, replace identation tabs with spaces

--- generators/app/templates/src/CppProject/MyClass.cpp
+++ generators/app/templates/src/CppProject/MyClass.cpp
@@ -1,105 +1,100 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///
-///	Copyright (C) <%= year %> by <%= companyName %>
+///	Copyright (C) 2017 by Company Inc.
 ///
-///	This file is part of the <%= projectName %> project.
+///	This file is part of the C++ Project project.
 ///
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///	@file						<%= projectPath %>/MyClass.cpp
-///	@author						<%= authorName %> <<%= authorEmail %>>
-///	@date						<%= date %>
+///	@file						CppProject/MyClass.cpp
+///	@author						Bob Marley <bob@marley.com>
+///	@date						1 Dec 2017
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 
-#include <<%= projectPath %>/MyClass.hpp>
+#include <CppProject/MyClass.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-namespace <%= projectPath.toLowerCase() %>
+namespace cppproject
 {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-								MyClass::MyClass							( )
-								:	enum_(MyClass::MyEnum::First),
-									integer_(0),
-									string_("")
+                                MyClass::MyClass							( )
+                                :	enum_(MyClass::MyEnum::First),
+                                    integer_(0),
+                                    string_("")
 {
 
 }
 
-								MyClass::MyClass							(	const	MyClass::MyEnum&			anEnum,
-																				const	int&						anInteger,
-																				const	std::string&				aString								)
-								:	enum_(anEnum),
-									integer_(anInteger),
-									string_(aString)
+                                MyClass::MyClass							(	const	MyClass::MyEnum&			anEnum,
+                                                                                const	int&						anInteger,
+                                                                                const	std::string&				aString								)
+                                :	enum_(anEnum),
+                                    integer_(anInteger),
+                                    string_(aString)
 {
 
 }
 
 bool							MyClass::operator ==						(	const	MyClass&					aMyClass							) const
 {
-	return (enum_ == aMyClass.enum_) && (integer_ == aMyClass.integer_) && (string_ == aMyClass.string_) ;
+    return (enum_ == aMyClass.enum_) && (integer_ == aMyClass.integer_) && (string_ == aMyClass.string_) ;
 }
 
 std::ostream&					operator <<									(			std::ostream&				anOutputStream,
-																				const	MyClass&					aMyClass							)
+                                                                                const	MyClass&					aMyClass							)
 {
 
-	anOutputStream																<<		"-- MyClass ----------------------------------------------------" << std::endl ;
+    anOutputStream << "Greetings!" << std::endl ;
 
-	anOutputStream																<<		"\tInteger:\t" << aMyClass.integer_ ;
-	anOutputStream																<<		"\tString:\t" << aMyClass.string_ ;
-
-	anOutputStream																<<		"---------------------------------------------------------------" << std::endl ;
-
-	return anOutputStream ;
+    return anOutputStream ;
 
 }
 
 bool							MyClass::isDefined							( ) const
 {
-	return integer_ > 0 ;
+    return integer_ > 0 ;
 }
 
 int								MyClass::getInteger							( ) const
 {
-	return integer_ ;
+    return integer_ ;
 }
 
 void							MyClass::setInteger							(	const	int&						anInteger							)
 {
-	integer_																	=		anInteger ;
+    integer_ = anInteger ;
 }
 
 void							MyClass::doSomething						( )
 {
-	this->doSomethingPrivate() ;
+    this->doSomethingPrivate() ;
 }
 
 void							MyClass::printSomething						( )
 {
-	std::cout																	<<		"Hello World!" << std::endl ;
+    std::cout << "Hello World!" << std::endl ;
 }
 
 MyClass							MyClass::Integer							(	const	int&						anInteger							)
 {
 
-	MyClass						myClass ;
+    MyClass myClass ;
 
-	myClass.integer_															=		anInteger ;
+    myClass.integer_ = anInteger ;
 
-	return myClass ;
+    return myClass ;
 
 }
 
 void							MyClass::doSomethingPrivate					( )
 {
-	integer_																	=		integer_ * 2 ;
+    integer_ = integer_ * 2 ;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- generators/app/templates/test/CppProject/MyClass.test.cpp
+++ generators/app/templates/test/CppProject/MyClass.test.cpp
@@ -1,70 +1,205 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///
-///	Copyright (C) <%= year %> by <%= companyName %>
+///	Copyright (C) 2017 by Company Inc.
 ///
-///	This file is part of the <%= projectName %> project.
+///	This file is part of the C++ Project project.
 ///
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///	@file						<%= projectPath %>/MyClass.test.cpp
-///	@author						<%= authorName %> <<%= authorEmail %>>
-///	@date						<%= date %>
+///	@file						CppProject/MyClass.test.cpp
+///	@author						Bob Marley <bob@marley.com>
+///	@date						1 Dec 2017
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Global.test.hpp>
 
-#include <<%= projectPath %>/MyClass.hpp>
+#include <CppProject/MyClass.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(<%= projectPath %>_MyClass, IsDefined)
+TEST(CppProject_MyClass, DefaultConstructor)
 {
 
-	using <%= projectPath.toLowerCase() %>::MyClass ;
+    using cppproject::MyClass ;
 
-	{
-
-		MyClass myClass ;
-
-		EXPECT_FALSE(myClass.isDefined()) ;
-
-	}
-
-	{
-
-		MyClass myClass = MyClass::Integer(123) ;
-
-		EXPECT_TRUE(myClass.isDefined()) ;
-
-	}
+    EXPECT_NO_THROW(MyClass()) ;
 
 }
 
-TEST(<%= projectPath %>_MyClass, DoSomething)
+TEST(CppProject_MyClass, Constructor)
 {
 
-	using <%= projectPath.toLowerCase() %>::MyClass ;
+    using cppproject::MyClass ;
 
-	{
+    EXPECT_NO_THROW(MyClass(MyClass::MyEnum::First, 123, "Hello World!")) ;
 
-		MyClass myClass ;
+}
 
-		myClass.doSomething() ;
+TEST(CppProject_MyClass, EqualToOperator)
+{
+    
+    using cppproject::MyClass ;
 
-		EXPECT_EQ(0, myClass.getInteger()) ;
+    EXPECT_TRUE(MyClass() == MyClass()) ;
+    EXPECT_TRUE(MyClass(MyClass::MyEnum::First, 123, "Hello World!") == MyClass(MyClass::MyEnum::First, 123, "Hello World!")) ;
 
-	}
+    EXPECT_FALSE(MyClass(MyClass::MyEnum::First, 123, "Hello World!") == MyClass(MyClass::MyEnum::Second, 123, "Hello World!")) ;
+    EXPECT_FALSE(MyClass(MyClass::MyEnum::First, 123, "Hello World!") == MyClass(MyClass::MyEnum::First, 456, "Hello World!")) ;
+    EXPECT_FALSE(MyClass(MyClass::MyEnum::First, 123, "Hello World!") == MyClass(MyClass::MyEnum::First, 123, "Hello You!")) ;
 
-	{
+}
 
-		MyClass myClass = MyClass::Integer(123) ;
+TEST(CppProject_MyClass, StreamOperator)
+{
 
-		myClass.doSomething() ;
+    using cppproject::MyClass ;
 
-		EXPECT_EQ(246, myClass.getInteger()) ;
+    {
 
-	}
+        std::stringstream		stringStream ;
+
+        MyClass myClass ;
+
+        stringStream << myClass ;
+
+        EXPECT_EQ("Greetings!\n", stringStream.str()) ;
+
+    }
+
+}
+
+TEST(CppProject_MyClass, IsDefined)
+{
+
+    using cppproject::MyClass ;
+
+    {
+
+        MyClass myClass ;
+
+        EXPECT_FALSE(myClass.isDefined()) ;
+
+    }
+
+    {
+
+        MyClass myClass = MyClass::Integer(123) ;
+
+        EXPECT_TRUE(myClass.isDefined()) ;
+
+    }
+
+}
+
+TEST(CppProject_MyClass, GetInteger)
+{
+
+    using cppproject::MyClass ;
+
+    {
+
+        MyClass myClass ;
+
+        EXPECT_EQ(0, myClass.getInteger()) ;
+
+    }
+
+    {
+
+        MyClass myClass = MyClass::Integer(123) ;
+
+        EXPECT_EQ(123, myClass.getInteger()) ;
+
+    }
+
+}
+
+TEST(CppProject_MyClass, SetInteger)
+{
+
+    using cppproject::MyClass ;
+
+    {
+
+        MyClass myClass ;
+
+        EXPECT_EQ(0, myClass.getInteger()) ;
+
+        myClass.setInteger(123) ;
+        
+        EXPECT_EQ(123, myClass.getInteger()) ;
+
+    }
+
+    {
+
+        MyClass myClass = MyClass::Integer(123) ;
+
+        EXPECT_EQ(123, myClass.getInteger()) ;
+
+        myClass.setInteger(456) ;
+        
+        EXPECT_EQ(456, myClass.getInteger()) ;
+
+    }
+
+}
+
+TEST(CppProject_MyClass, DoSomething)
+{
+
+    using cppproject::MyClass ;
+
+    {
+
+        MyClass myClass ;
+
+        myClass.doSomething() ;
+
+        EXPECT_EQ(0, myClass.getInteger()) ;
+
+    }
+
+    {
+
+        MyClass myClass = MyClass::Integer(123) ;
+
+        myClass.doSomething() ;
+
+        EXPECT_EQ(246, myClass.getInteger()) ;
+
+    }
+
+}
+
+TEST(CppProject_MyClass, PrintSomething)
+{
+
+    using cppproject::MyClass ;
+
+    {
+        
+        MyClass myClass ;
+
+        EXPECT_NO_THROW(myClass.printSomething()) ;
+
+    }
+
+}
+
+TEST(CppProject_MyClass, Integer)
+{
+    
+    using cppproject::MyClass ;
+
+    {
+
+        MyClass myClass = MyClass::Integer(123) ;
+
+        EXPECT_EQ(123, myClass.getInteger()) ;
+
+    }
 
 }
 
